94a5040219620ee8a40757ad03162768fd1b38fb
Mikolaj Myszka <mikolaj.myszka@stxnext.pl>
2019-11-25 17:01:30 +0100

    mon2511

---
+++ logging1.py
@@ -0,0 +1,47 @@
+import logging
+import math
+
+# print(dir(logging))
+
+# Create and configure logger
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+logging.basicConfig(filename="lumberjack.log",
+                    level=logging.DEBUG, 
+                    format=LOG_FORMAT, 
+                    filemode="w")
+logger = logging.getLogger()
+
+"""
+# Test the logger
+logger.debug("Debug mode.")
+logger.info("Info mode.")
+logger.warning("Warning mode.")
+logger.error("Error mode.")
+logger.critical("Critical mode.")
+
+print(logger.level)
+"""
+
+def quadratic_formula(a, b, c):
+    """Return the solution to the equation ax^2 + bx + c = 0."""
+    logger.info("quadratic_formula({0}, {1}, {2})".format(a, b, c))
+
+    # Compute the discriminant
+    logger.debug("# Compute the discriminant.")
+    disc = b^2 - 4*a*c
+
+    # Compute the 2 roots
+    logger.debug("# Compute the 2 roots")
+    root1 = (-b + math.sqrt(disc)) / (2*a)
+    root2 = (-b - math.sqrt(disc)) / (2*a)
+
+    # Return the roots (as a tuple)
+    logger.debug("# Return the roots")
+    return (root1, root2)
+
+
+roots = quadratic_formula(1, 0, -4)
+#roots = quadratic_formula(1, 0, 1)
+print(roots)
+
+
--- re1.py
+++ re1.py
@@ -22,7 +22,7 @@ Mr. T
 
 sentence = 'Start a sentence and then bring it to an end'
 
-pattern = re.compile(r'coreyms\.com')
+pattern = re.compile(r'[89]00[-.]\d\d\d[-.]\d\d\d\d')
 
 matches = pattern.finditer(text_to_search)
 
@@ -30,3 +30,42 @@ for match in matches:
     print(match)
 
 print(text_to_search[1:4])
+
+
+
+# Let's create a pattern to match these numbers: 321-555-4321 and 123.555.1234
+# pattern = re.compile(r'\d\d\d.\d\d\d.\d\d\d\d')
+
+# pattern = re.compile(r'[89]00[-.]\d\d\d[-.]\d\d\d\d')
+
+"""
+.       - Any Character Except New Line
+\d      - Digit (0-9)
+\D      - Not a Digit (0-9)
+\w      - Word Character (a-z, A-Z, 0-9, _)
+\W      - Not a Word Character
+\s      - Whitespace (space, tab, newline)
+\S      - Not Whitespace (space, tab, newline)
+
+\b      - Word Boundary
+\B      - Not a Word Boundary
+^       - Beginning of a String
+$       - End of a String
+
+[]      - Matches Characters in brackets
+[^ ]    - Matches Characters NOT in brackets
+|       - Either Or
+( )     - Group
+
+Quantifiers:
+*       - 0 or More
++       - 1 or More
+?       - 0 or One
+{3}     - Exact Number
+{3,4}   - Range of Numbers (Minimum, Maximum)
+
+
+#### Sample Regexs ####
+
+[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+
+"""
