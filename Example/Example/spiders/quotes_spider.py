c50127f55d1268c20ac1a53d98a702811e3b5c8f
Mikolaj Myszka <mikolaj.myszka@stxnext.pl>
2019-12-10 17:17:47 +0100

    tue1012

--- Example/Example/spiders/quotes_spider.py
+++ Example/Example/spiders/quotes_spider.py
@@ -7,5 +7,30 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        """ # Example 1
         title = response.css('title::text').extract()
         yield {'title_text': title}
+        """
+        """
+        all_div_quotes = response.css('div.quote')
+        title = all_div_quotes.css('span.text::text').extract()
+        author = all_div_quotes.css('.author::text').extract()
+        tag = all_div_quotes.css('a.tag::text').extract()
+        yield {
+            'title': title,
+            'author': author,
+            'tag': tag
+        }
+        """
+        # Example of getting data one by one
+        all_div_quotes = response.css('div.quote')
+
+        for quotes in all_div_quotes:
+            title = quotes.css('span.text::text').extract()
+            author = quotes.css('.author::text').extract()
+            tag = quotes.css('a.tag::text').extract()
+            yield {
+                'title': title,
+                'author': author,
+                'tag': tag
+            }
