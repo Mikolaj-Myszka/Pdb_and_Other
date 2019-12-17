659ab8758d4df333c8864e3bc9be13b11e56a6f4
Mikolaj Myszka <mikolaj.myszka@stxnext.pl>
2019-12-17 17:18:56 +0100

    tue1712

--- Example/Example/pipelines.py
+++ Example/Example/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-import sqlite3
+import mysql.connector
 
 class ExamplePipeline(object):
 
@@ -14,7 +14,12 @@ class ExamplePipeline(object):
         self.create_table()
 
     def create_connection(self):
-        self.conn = sqlite3.connect('myquotes.db')
+        self.conn = mysql.connector.connect(
+            host = 'localhost',
+            user = 'root',
+            passwd = '1helloworld',
+            database = 'myquotes'
+        )
         self.curr = self.conn.cursor()
 
     def create_table(self):
@@ -30,7 +35,7 @@ class ExamplePipeline(object):
         return item
 
     def store_db(self, item):
-        self.curr.execute("""insert into quotes_tb values(?, ?, ?)""", (
+        self.curr.execute("""insert into quotes_tb values(%s, %s, %s)""", (
             item['title'][0],
             item['author'][0],
             item['tag'][0]
