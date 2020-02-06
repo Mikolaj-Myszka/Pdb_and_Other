b849826355243a560f3bab5680d034fa3441fd7c
Mikolaj Myszka <mikolaj.myszka@stxnext.pl>
2020-02-06 11:41:41 +0100

    0602

--- zadanko.py
+++ zadanko.py
@@ -1,11 +1,11 @@
 # Case1
-def fun(s_arg=[]):
-    s_arg.append('Marcin')
-    return s_arg
+# def fun(s_arg=[]):
+#     s_arg.append('Marcin')
+#     return s_arg
 
-print(fun())
-print(fun([]))
-print(fun())
+# print(fun())
+# print(fun([]))
+# print(fun())
 
 
 # Case2
@@ -22,3 +22,41 @@ print(fun())
 # a += [3,4]
 # print(a)
 # print(b)
+
+
+# Case4
+# print("text" is not None)
+# print("text" is (not None))
+# print(not None) # <- zwraca True
+
+# Case5
+# a = []
+# b = []
+# print(a is b)
+
+# a = ()
+# b = ()
+# print(a is b)
+
+# Case6
+# True = False
+# if True == False:
+#     print('Miki')
+
+# Case7
+# a,b = a[b] = {},5
+# print(a)
+
+# Case8
+# a = [0]
+# a.append(a)
+# print(a)
+
+# Case9
+snt = snt[0] = [0]
+print(snt)
+print(snt[0])
+print(snt[0] == snt)
+print(snt[0][0][0][0])
+print(snt[0][0][0][0][0][0])
+
